4be79fd94cfb0a6d1331b362d600ae3c709c7d77
Ashik-M-S <msashik2005@gmail.com>
2026-06-29 11:17:51 +0530

    Updated API URL

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://caretrack-b5lk.onrender.com/api';
 
 async function callApi(endpoint, method = 'GET', body = null) {
     const user = JSON.parse(localStorage.getItem('user'));
